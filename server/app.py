a7c2a8c38b789baf61292d334a6f2312d3949e45
CAROLKOECH <koechc44@gmail.com>
2023-10-01 20:27:27 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
-from flask import Flask, make_response
+from flask import Flask
 from flask_migrate import Migrate
-
 from models import db, Author, Post
 
 app = Flask(__name__)
--- server/models.py
+++ server/models.py
@@ -1,23 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+import re
+
 db = SQLAlchemy()
 
 class Author(db.Model):
     __tablename__ = 'authors'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
-    phone_number = db.Column(db.String)
+    name = db.Column(db.String, unique=True, nullable=False)
+    phone_number = db.Column(db.String(10))  # Ensure 10 digits
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    def __repr__(self):
-        return f'Author(id={self.id}, name={self.name})'
+    @validates('name')
+    def validate_name(self, key, value):
+        # Ensure all authors have a name
+        if not value:
+            raise ValueError("Author must have a name")
+        return value
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -27,6 +31,39 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('title')
+    def validate_title(self, key, value):
+        # Ensure all posts have a title
+        if not value:
+            raise ValueError("Post must have a title")
+        return value
 
-    def __repr__(self):
-        return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+    @validates('content')
+    def validate_content_length(self, key, value):
+        # Ensure post content is at least 250 characters long
+        if len(value) < 250:
+            raise ValueError("Post content must be at least 250 characters long")
+        return value
+
+    @validates('summary')
+    def validate_summary_length(self, key, value):
+        # Ensure post summary is at most 250 characters long
+        if len(value) > 250:
+            raise ValueError("Post summary cannot exceed 250 characters")
+        return value
+
+    @validates('category')
+    def validate_category(self, key, value):
+        # Ensure post category is either 'Fiction' or 'Non-Fiction'
+        if value not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Invalid post category")
+        return value
+
+    @validates('title')
+    def validate_clickbait_title(self, key, value):
+        # Ensure the title is sufficiently clickbait-y
+        clickbait_phrases = ["Won't Believe", "Secret", "Top [0-9]+", "Guess"]
+        pattern = '|'.join(clickbait_phrases)
+        if not re.search(pattern, value):
+            raise ValueError("Post title is not clickbait-y enough")
+        return value
